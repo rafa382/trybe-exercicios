5d73dd1bdae87ee2af71479e0af45dd1fd748376
Rafaela Camargos <rafaela.camargos.ribeiro@gmail.com>
2021-08-26 11:20:44 -0300

    Adiciona atualizações dos exercicios 1 e 2  dos conteúdos.

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exercicio-2-conteudo.js
@@ -0,0 +1,29 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
+
+patientId = '50';
+
+console.log(typeof patientId);
+
+//exercício Operadores Aritméticos
+
+const base = 5;
+let height = 8;
+
+console.log(base*height);
+
+const area = 40;
+
+const perimeter = base + height + area;
+console.log(perimeter);
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/exercicio-3-conteudo.js
@@ -0,0 +1,9 @@
+const notaCandidato = 90;
+
+if (notaCandidato>=80){
+    console.log("Parabéns,você foi aprovada(o)!")
+}else if(notaCandidato<60){
+    console.log("Você foi reprovada(o)")
+}else{
+ console.log("Recuperação")   
+}
